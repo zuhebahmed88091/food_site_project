1e5ccaaf7146b06ead5e9979414efc5ffb65ec2c
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-10-07 10:39:06 +0600

    feat:Add __str__ function in food/models.py and by python shell in cmd we created two data and stored in our database table named Item. To do that we had to create Item class in models.py

--- food/models.py
+++ food/models.py
@@ -7,3 +7,6 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_description = models.CharField(max_length=200)
     item_price = models.IntegerField()
+
+    def __str__(self):
+        return self.item_name
