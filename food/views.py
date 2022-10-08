3b2de884caaa195c1d5786c94d4a3f0e1b2d8f9e
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-10-08 08:56:12 +0600

    feat: To retrieve data we add Item.objects.all() and saved it into item_list in our food/views.py at our index page/class. Before that we had to import Item model. Then we pass the item_list in HttpResponse.

--- food/views.py
+++ food/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello')
+    item_list = Item.objects.all()
+    return HttpResponse(item_list)
 
 
 def item(request):
