dfcfbc75748ca70d8e174b02fcc8165d75d1b2f6
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-10-08 11:37:24 +0600

    feat: To add template we create directory named templates in food(app) then create another directory named food under templates. Then we created our index.html file and add sample html code for rendering. After thatin food/views.py at def index(request): we load the template by loader and created an empty context. For loader we had to import from django.template import loader. Then we render the index.html page by return HttpResponse(template.render(context, request)). Here template variable is that variable where we load our template.

--- food/views.py
+++ food/views.py
@@ -1,13 +1,19 @@
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
+from django.template import loader
 # Create your views here.
 
 
 def index(request):
     item_list = Item.objects.all()
-    return HttpResponse(item_list)
+    template = loader.get_template('food/index.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def item(request):
