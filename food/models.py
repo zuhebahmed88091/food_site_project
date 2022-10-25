b686b36c0fdd3c63e692382ca368ef480184656f
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-10-25 10:49:33 +0600

    feat: We are going to add image field to our model. To do that first we have to add a field in food/models.py like item_image=models.Charfield(max_length=500, default='') in default we have to add a default image link. After that we have to migrate our model and we already know the process. Update: To display our image in index.html we remove our code which was in our {% block body %} and {%for item in item_list %} and replaced by theree <div> in which we add image,name,description,price and in our last div we add a link button in which we add the link of detail.html. That means when we click the button we can view item details.

--- food/models.py
+++ food/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
@@ -7,6 +8,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_description = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default="https://assets.materialup.com/uploads/b03b23aa-aa69-4657-aa5e-fa5fef2c76e8/preview.png")
 
     def __str__(self):
         return self.item_name
