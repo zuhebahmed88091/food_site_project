dba0f1db5b6366aa71d10608b9364929b72ff533
zuhebahmed88091 <zuhebahmed88091@gmail.com>
2022-12-22 11:22:26 +0600

    To add items by form firstly we need to set up url. In food/url.py we add path(add, views.create_item, name=create_item) after that we need to create an extra thing which is called forms.py in food(app).This forms.py is going to havea class for each form which we are creating.In there we import froms and Item model and create ItemForm class which inherits from model forms.After that we need to create Meta class which is going to hold information about what field should be present in that particular form.After that we have to create template for the form.In food(app)/templates/food we create item-form.html.In there in order to create form we extends base.html and create <form> which has POST method, csrf_token and {{form}} which is just context which will be passed from views.After that in views.py we created a view named create_item which was already mentioned in urls.py and in there we created the object named form of ItemForm class and check the validity of the form and saved it.After that we redirect in the index page after submitting the form and lastly we render the item-form.html and pass the form context and we also import redirect and ItemForm in views.py.In base.html in navbar part we add url to the Add Item section and the url is our form add item url.

--- food/urls.py
+++ food/urls.py
@@ -8,5 +8,6 @@ app_name = 'food'
 urlpatterns = [
     path('', views.index, name='index'),
     path('item/', views.item, name='item'),
-    path('<int:item_id>/', views.detail, name='detail')
+    path('<int:item_id>/', views.detail, name='detail'),
+    path('add', views.create_item, name='create_item')
 ]
--- food/views.py
+++ food/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Item
 from django.template import loader
+from .forms import ItemForm
 # Create your views here.
 
 
@@ -24,3 +25,12 @@ def detail(request, item_id):
         'item':item,
     }
     return render(request, 'food/detail.html', context)
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+    
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    
+    return render(request, 'food/item-form.html', {'form':form})
